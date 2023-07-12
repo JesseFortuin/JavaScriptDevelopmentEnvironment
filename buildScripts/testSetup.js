e5f1b15b0f56450ac4fba4b7ea6e2f2dfb60f403
jesse <jessefortuin850@gmail.com>
2023-07-12 12:54:42 +0200

    Feat: testing with mocha

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,5 @@
+//Register babel to transpile before tests are run
+require('@babel/register')();
+
+//Disable webpack features that mocha doesn't understand.
+require.extensions['.css'] = function() {};
